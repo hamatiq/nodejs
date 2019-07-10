f84746bc970fb91579ba4993ac2a1ed2415cb196
hamatiq <44220880+hamatiq@users.noreply.github.com>
2019-07-10 04:51:16 -0500

    npm packages

--- node-udemy/notes-app/notes-app.js
+++ node-udemy/notes-app/notes-app.js
@@ -1,5 +1,9 @@
-const notes = require('./notes.js')
+const chalk = require('../../node_modules/chalk');
+
+const notes = require('./notes.js');
 
 const contant = notes('notes.txt');
 
 console.log(contant);
+
+console.log(chalk.green('hello world'));
