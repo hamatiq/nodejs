1ad1ba9dd97e79f6a9f7614be2a87763e5a766e7
hamatiq <44220880+hamatiq@users.noreply.github.com>
2019-07-12 04:33:01 -0500

    hi

--- node-udemy/notes-app/notes-app.js
+++ node-udemy/notes-app/notes-app.js
@@ -1,16 +1,48 @@
 const chalk = require('../../node_modules/chalk');
-
+const yargs = require('../../node_modules/yargs');
 const notes = require('./notes.js');
 
 const contant = notes('notes.txt');
 
 const command = process.argv[2];
+// customize yarg
+yargs.version('1.1.0');
 
-console.log(process.argv);
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    handler: function() {
+        console.log('adding a new note!');
+    }
+});
 
 
-if (command == 'add') {
-    console.log('Adding note!');
-} else if (command == 'remove') {
-    console.log('removeing note!');
-}
+// remove, read, list commands
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function() {
+        console.log('removing a note note!');
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'list all notes',
+    handler: function() {
+        console.log('listing notes');
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function() {
+        console.log('reating a note!');
+    }
+});
+
+// add, remove, read, list
+
+console.log(yargs.argv);
